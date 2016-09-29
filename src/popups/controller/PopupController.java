b4e022d4ca2db8f5283c79d5acc8a3e367075624
jackopuff <jcar8281@740s-pl-128.local>
2016-09-29 12:40:23 -0600

    Added a verification to convert integer values.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -24,20 +24,44 @@ public class PopupController
 		 * Checks a supplied String value to see if it can be converted
 		 * into a double.
 		 * @param potentialValue The supplied String.
-		 * @return Whether the conversion was successful as a boolean value.
+		 * @return Whether the conversion was successful (true/false).
 		 */
 		private boolean isDouble(String potentialValue)
 		{
-		Boolean validDouble = false;
-		try
-		{
-			double testValue = Double.parseDouble(potentialValue);
-			validDouble = true;
+			Boolean validDouble = false;
+			try
+			{
+				double testValue = Double.parseDouble(potentialValue);
+				validDouble = true;
+			}
+			catch(NumberFormatException notDoubleError)
+			{
+				display.displayMessage("That was nota  double XD");
+			}
+			return validDouble;
 		}
-		catch(NumberFormatException notDoubleError)
+		
+		/**
+		 * Checks supplied String value to see if it can be converted
+		 * into an integer. If it can't, a popup is displayed.
+		 * @param potentialValue The supplied String.
+		 * @return Whether the conversion was successful (true/false).
+		 */
+		
+		private boolean isInteger(String potentialValue)
 		{
-			display.displayMessage("That was nota  double XD");
-		}
-		return validDouble;
+			boolean validInteger = false;
+			try
+			{
+				int testValue = Integer.parseInt(potentialValue);
+				validInteger = true;
+			}
+			catch(NumberFormatException notIntegerError)
+			{
+				display.displayMessage(notIntegerError.getMessage());
+				display.displayMessage("you need an int");
+			}
+			
+			return validInteger;
 		}
 }
